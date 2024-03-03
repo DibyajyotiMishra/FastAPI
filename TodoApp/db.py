8ad6a7c8888c4cb3ecd55bd5363a804cb16eb624
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2024-03-03 10:28:05 +0530

    add postgresql connection

--- TodoApp/db.py
+++ TodoApp/db.py
@@ -1,10 +1,16 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/ToDoAppDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+# def _fk_pragma_on_connect(dbapi_con, con_record):
+#     dbapi_con.execute('pragma foreign_keys=ON')
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# event.listen(engine, 'connect', _fk_pragma_on_connect)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
