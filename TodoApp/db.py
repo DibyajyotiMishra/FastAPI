c66638f949bd1215f76353191995f85af22a5c74
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2024-03-12 23:32:21 +0530

    chore: clean up db.py

--- TodoApp/db.py
+++ TodoApp/db.py
@@ -4,13 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/ToDoApplication'
 
-
-# def _fk_pragma_on_connect(dbapi_con, con_record):
-#     dbapi_con.execute('pragma foreign_keys=ON')
-
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# event.listen(engine, 'connect', _fk_pragma_on_connect)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
